412cbeda346ec37746b9860e649c4833b70d3c23
tyxchen <tyxchen@outlook.com>
2019-03-10 12:11:11 -0400

    Add events viewer

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import { $, $$ } from "@js/modules/utils.js";
+import { RecentEventViewer } from "@js/modules/events.js";
 import Tabber from "@js/modules/tabber.js";
 
 import '@css/index.scss';
@@ -37,6 +38,76 @@ $('#main-nav').addEventListener('click', function(e) {
 
 for (const tabber of $$('.tabber')) {
   new Tabber(tabber);
+}
+
+if (window.events) {
+  const viewer = new RecentEventViewer(window.events);
+
+  if ($('#home-events-toast')) {
+    const events = viewer.getRecentEvents();
+    console.dir(events);
+    
+    if (events.ongoing.length) {
+      if (events.ongoing.length > 1) {
+        $('#home-events-toast .container').classList.add('animated');
+      }
+      $('#home-events-toast .container').insertAdjacentHTML('beforeend',
+        `<strong><span class="live"></span> Ongoing:</strong>
+<ul id="home-events-toast-marquee">
+  ${events.ongoing.map((e, i) => `<li style="animation-duration:${6*events.ongoing.length}s;animation-delay:${6*i}s;">
+    <a><em>${e.Title}</em> <span class="slash-sep">//</span> ${e.Location || e.Locations.join(', ')}</a>
+  </li>`).join('')}
+</ul>`);
+    } else if (events.upcoming.length) {
+      if (events.upcoming.length > 1) {
+        $('#home-events-toast .container').classList.add('animated');
+      }
+      $('#home-events-toast .container').insertAdjacentHTML('beforeend',
+        `<strong>Next Event:</strong>
+<ul id="home-events-toast-marquee">
+  ${events.upcoming.map((e, i) => `<li style="animation-duration:${6*events.upcoming.length}s;animation-delay:${6*i}s;">
+    <a><em>${e.Title}</em> <span class="slash-sep">//</span> ${e.formattedShortDate} ${e.Time} <span class="slash-sep">//</span> ${e.Location || e.Locations.join(', ')}</a>
+  </li>`).join('')}
+</ul>`)
+    } else {
+      $('#home-events-toast .container').style.visibility = 'hidden';
+    }
+
+    $('#home-events-toast').insertAdjacentHTML('beforeend', `<style>
+@-webkit-keyframes home-events-marquee {
+  from {
+    opacity: 0;
+    -webkit-transform: translateY(100%);
+  }
+
+  ${30/(6*(events.ongoing.length || events.upcoming.length || 1))}%, ${570/(6*(events.ongoing.length || events.upcoming.length || 1))}% {
+    opacity: 1;
+    -webkit-transform: translateY(0);
+  }
+
+  ${600/(6*(events.ongoing.length || events.upcoming.length || 1))}% {
+    opacity: 0;
+    -webkit-transform: translateY(-100%);
+  }
+}
+@keyframes home-events-marquee {
+  from {
+    opacity: 0;
+    transform: translateY(100%);
+  }
+
+  ${30/(6*(events.ongoing.length || events.upcoming.length || 1))}%, ${570/(6*(events.ongoing.length || events.upcoming.length || 1))}% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+
+  ${600/(6*(events.ongoing.length || events.upcoming.length || 1))}% {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+}
+</style>`)
+  }
 }
 
 console.log(atob("ICAgICAgICAgICAgICAgICAgICAgICBfXwogICAgICAgIC9cICAgICAgICAgICAgfCAgfCAgICAgICAgICAgfCAgfCAgICAgfCB8ICAgICAgICAgICAgICAgICAgICAgIHwKICAgICAgIC8gIFwgICAgICAgICAgIHwgIHwgICAgICAgICAgIHxfX3wgIF8gIHwgfCAgICogIF8gICAgLF8gIF8gICBfICB8CiAgICAgIC8gICAgXCAgICAgICAgICB8ICB8ICAgICAgICAgICB8ICB8IC9fXCB8IHwgICB8IC9fYCAgIHwgIC9fXCAvIHwgfAogICAgIC8gICAgICBcICAgICAgICAgfCAgfCAgICAgICAgICAgfCAgfCBcXyAgXCBcICAgfCAuXy8gICB8ICBcXyAgXF98IFwgIwogICAgLyAgICAgICAgXCAgICAgICAgfCAgfCAgICAgCiAgIHwgIC98ICB8XCAgfCAgICAgICB8ICB8ICAgICAgICAgICB8ICB8ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIHwKICAgfCAvIHwgIHwgXCB8ICAgICAgIHwgIHwgICAgICAgICAgIHxfX3wgIF8gICBfICAgICAgICBfICAsXyAgICAqICBfICAgICxfICBfICAgXyAgfAogICB8LyAgfCAgfCAgXHwgICAgICAgfCAgfCAgICAgICAgICAgfCAgfCAvX1wgLyB8IFwgIC8gL19cIHwgfCAgIHwgL19gICAgfCAgL19cIC8gfCB8CiAgICAgICB8ICB8ICAgICAgICAgICB8ICB8ICAgICAgICAgICB8ICB8IFxfICBcX3wgIFwvICBcXyAgfCB8ICAgfCAuXy8gICB8ICBcXyAgXF98IFwgIwogICAgICAgfCAgfCAgICAgICAgICAgfCAgfCAgICAgICAgICAgX19fXwogICAgICAgfCAgfCAgICAgICB8XCAgfCAgfCAgL3wgICAgICAgIHx8ICAgICAgICAgICAgICAgICAgICAgICAgICAgIHwgICAgICAgICAgIHwKICAgICAgIHwgIHwgICAgICAgfCBcIHwgIHwgLyB8ICAgICAgICB8fCAgKiAsXyBfICAgXyAgICAqICBfICAgICBfICB8XyAgIF8gICxfIF98XwogICAgICAgfCAgfCAgICAgICB8ICBcfCAgfC8gIHwgICAgICAgIHx8ICB8IHwgfCB8IC9fXCAgIHwgL19gICAgL19gIHwgfCAvIFwgfCAgIHwKICAgICAgIHwgIHwgICAgICAgXCAgICAgICAgICAvICAgICAgICB8fCAgfCB8IHwgfCBcXyAgICB8IC5fLyAgIC5fLyB8IHwgXF8vIHwgICBcICMKICAgICAgIHwgIHwgICAgICAgIFwgICAgICAgIC8gICAgICAgICBfXwogICAgICAgfCAgfCAgICAgICAgIFwgICAgICAvICAgICAgICAgLyAgIHwKICAgICAgIHwgIHwgICAgICAgICAgXCAgICAvICAgICAgICAgIHwgICB8XyAgIF8gICBfICAgXyAgIF8KICAgICAgIHwgIHwgICAgICAgICAgIFwgIC8gICAgICAgICAgIHwgICB8IHwgLyBcIC8gXCAvX2AgL19cCiAgICAgICB8X198ICAgICAgICAgICAgXC8gICAgICAgICAgICBcX18gfCB8IFxfLyBcXy8gLl8vIFxfICAj"))
---
+++ src/js/modules/events.js
@@ -0,0 +1,82 @@
+export class EventViewer {
+  constructor(json) {
+    this.events = json;
+  }
+
+  format(date) {
+    return `${('0' + (date.getMonth() + 1)).slice(-2)}.${('0' + date.getDate()).slice(-2)}.${date.getFullYear()}`;
+  }
+
+  formatShort(date) {
+    return ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][date.getDay()] + '.';
+  }
+
+  render(templ) {
+    return templ.replace(/#\{(\w+)\.(\w+)\}/g, (_, m, ev, sub) => this.events[ev][sub]);
+  }
+}
+
+export class RecentEventViewer extends EventViewer {
+  constructor(json, today = new Date('March 10 2019 19:00')) {
+    super(json);
+
+    this.event = null;
+    this.recentEvents = {
+      ongoing: [],
+      upcoming: []
+    }
+
+    let latestDate = new Date(2 ** 32 * 1000);
+
+    for (const id in this.events) {
+      const event = this.events[id],
+            times = event.Time.split('-')
+            .map(t => /^\d+$/.test(t) ? t + 'pm' : t)
+            .map(t => (parseInt(t.slice(0, 2)) + (~t.indexOf('pm') ? 12 : 0)) + ':' + (isNaN(parseInt(t.slice(2, 4))) ? '00' : t.slice(2, 4)));
+  
+      if (event.hasOwnProperty('Dates')) {
+        // find date closest to today
+        for (const d of event.Dates) {
+          const startTime = new Date(d + ' ' + times[0]),
+                endTime = new Date(d + ' ' + times[1]);
+
+          if (startTime <= today && today <= endTime) {
+            this.recentEvents.ongoing.push(Object.assign({}, event, {
+              formattedDate: this.format(startTime),
+              formattedShortDate: this.formatShort(startTime),
+            }));
+          } else if (startTime > today && startTime - today < 86400000) {
+            this.recentEvents.upcoming.push(Object.assign({}, event, {
+              formattedDate: this.format(startTime),
+              formattedShortDate: this.formatShort(startTime),
+            }));
+            break;
+          }
+        }
+      } else {
+        const startTime = new Date(event.Date + ' ' + times[0]),
+              endTime = new Date(event.Date + ' ' + times[1]);
+
+        if (startTime <= today && today <= endTime) {
+          this.recentEvents.ongoing.push(Object.assign({}, event, {
+            formattedDate: this.format(startTime),
+            formattedShortDate: this.formatShort(startTime),
+          }));
+        } else if (startTime > today && startTime - today < 86400000) {
+          this.recentEvents.upcoming.push(Object.assign({}, event, {
+            formattedDate: this.format(startTime),
+            formattedShortDate: this.formatShort(startTime),
+          }));
+        }
+      }
+    }
+  }
+
+  getRecentEvents() {
+    return this.recentEvents;
+  }
+
+  render(templ) {
+    return templ.replace(/#\{(\w+)\}/g, (_, m, sub) => this.events[this.event][sub]);
+  }
+}
